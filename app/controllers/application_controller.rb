3ff2cef67e34319e0a19e92ab3aeeb6fe592911a
Chris Potts <dev@icpu.co.uk>
2012-07-24 22:24:19 +0100

    restrict access

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,7 @@ class ApplicationController < ActionController::Base
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_path, :alert => exception.message
   end
+
+  before_filter :authenticate_user!
+
 end
