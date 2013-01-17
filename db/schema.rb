68162682d16cfb89703ad2c442940f8f4705fedb
Chris Potts <dev@icpu.co.uk>
2013-01-17 20:39:23 +0100

    before rails upgrade

--- db/schema.rb
+++ db/schema.rb
@@ -30,12 +30,6 @@ ActiveRecord::Schema.define(:version => 20120908122611) do
     t.integer  "user_id"
     t.integer  "ending_id"
     t.text     "content"
-    t.string   "name"
-    t.string   "email"
-    t.string   "site_url"
-    t.string   "user_ip"
-    t.string   "user_agent"
-    t.string   "referrer"
     t.datetime "created_at",                    :null => false
     t.datetime "updated_at",                    :null => false
     t.string   "ancestry"
